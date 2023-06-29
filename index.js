0f7d1debfd9ac2823f8979d5755ec60f098ae8a0
Sumeya Farah <136057548+Sumeiyah@users.noreply.github.com>
2023-06-29 19:28:28 +0300

    Done!

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+
+let random = Math.floor(Math.random(1) +1);
+
+let num3 = 104;
+let num4 = 25;
+let mod = num3%num4;
+
+
+let max = Math.max(19,20);
